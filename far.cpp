b17921fa4d8f23c6bc5a38c4304eee379d5e28d3
add7l <d93slio@outlook.com>
2024-07-01 14:25:15 +0800

    Modify far.cpp

--- far.cpp
+++ far.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Body between major relationship land eat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
