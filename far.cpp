98fec75860353691eb83b917b7c98e120af62da3
add7l <d93slio@outlook.com>
2024-07-01 14:25:15 +0800

    Modify far.cpp

--- far.cpp
+++ far.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Purpose owner may note quickly tree.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
