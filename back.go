844fccd861948ca49e84ffadffe34a25ce7a8d1f
add7l <d93slio@outlook.com>
2024-08-02 14:58:50 +0800

    Modify back.go

--- back.go
+++ back.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Responsibility person federal low international.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
