8bb6d79e92eaf0665432eb0b3f15c5a8a83658a2
add7l <d93slio@outlook.com>
2024-10-13 11:51:28 +0800

    Modify spring.cpp

--- spring.cpp
+++ spring.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Key model computer let real usually perform worker.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
