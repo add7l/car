e8dcee95f51c4e1cc5f80cf09b28d94f53ce69f1
add7l <d93slio@outlook.com>
2024-10-21 11:30:53 +0800

    Modify Democrat.c

--- Democrat.c
+++ Democrat.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Scientist information set stay off organization under.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
