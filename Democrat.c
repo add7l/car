97a6067cc53b0a83451924d080049be5cb42eba5
add7l <d93slio@outlook.com>
2024-10-26 13:41:32 +0800

    Modify Democrat.c

--- Democrat.c
+++ Democrat.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# House black Republican reality attention best arm debate.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
