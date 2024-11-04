695298c932fc00561ba256ba6d39931e76c7847c
add7l <d93slio@outlook.com>
2024-11-04 12:45:04 +0800

    Modify consider.c

--- consider.c
+++ consider.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Reach president actually state number lot reason.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
