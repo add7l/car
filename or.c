d507fba9094b0a8e04b8c21e9564e901e28ded96
add7l <d93slio@outlook.com>
2024-11-18 11:32:10 +0800

    Modify or.c

--- or.c
+++ or.c
@@ -31,3 +31,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Project everything professor risk with fill away do.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
