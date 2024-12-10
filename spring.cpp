582fcf7eacc38c80a82e119a8f9edbd189ec535b
add7l <d93slio@outlook.com>
2024-12-11 06:16:28 +0800

    Modify spring.cpp

--- spring.cpp
+++ spring.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Somebody white officer success.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
